79ebdc87f4c79499c1604463f76fd3046b5be847
giorgi <lipartelianige@gmail.com>
2026-03-13 23:59:12 +0400

    added request context to preload data from database and get have access to it from resolver classes

--- src/Controller/GraphQL.php
+++ src/Controller/GraphQL.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\GraphQL\Loaders\AttributeLoader;
+use App\GraphQL\Loaders\PriceLoader;
 use App\GraphQL\Types\SchemaTypes\MutationType;
 use App\GraphQL\Types\SchemaTypes\QueryType;
 use GraphQL\Error\DebugFlag;
@@ -52,8 +54,21 @@ class GraphQL
             $query = $input['query'];  // Extract the GraphQL query string
             $variables = $input['variables'] ?? null;  // Optional variables from request
 
+            // GraphQL request context.
+            // Loaders are instantiated once per request and shared across resolvers.
+            $context = [
+                'priceLoader' => new PriceLoader(),
+                'attributeLoader' => new AttributeLoader()
+            ];
+
             // Execute the GraphQL query
-            $result = GraphQLBase::executeQuery($schema, $query, null, null, $variables);
+            $result = GraphQLBase::executeQuery(
+                $schema,
+                $query,
+                null,
+                $context,
+                $variables
+            );
 
             // Convert execution result into array. Debug flag adds extra error information
             $output = $result->toArray(DebugFlag::INCLUDE_DEBUG_MESSAGE);
